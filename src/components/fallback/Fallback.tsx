53dcb6c91431e20ca47f649799276695bce6d6dd
Khaled Taymour <khaled.taymour@gmail.com>
2022-10-26 16:36:23 +0200

    Updates

--- src/components/fallback/Fallback.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ReactComponent as FallbackImage } from "assets/images/error-fallback.svg";
-
-const Fallback = () => (
-  <FallbackImage style={{ height: "200px", width: "200px" }} />
-);
-
-export default Fallback;
